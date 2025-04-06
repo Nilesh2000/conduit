53814cc892799c729fd7c335bf229e5c70f8b0e3
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-04-06 13:59:14 +0530

    define structs from openapi schema

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"time"
+)
+
+type LoginUser struct {
+	Email    string
+	Password string
+}
+
+type NewUser struct {
+	Username string
+	Email    string
+	Password string
+}
+
+type User struct {
+	Email    string
+	Token    string
+	Username string
+	Bio      string
+	Image    string
+}
+
+type UpdateUser struct {
+	Email    string
+	Token    string
+	Username string
+	Bio      string
+	Image    string
+}
+
+type Profile struct {
+	Username  string
+	Bio       string
+	Image     string
+	Following bool
+}
+
+type Article struct {
+	Slug           string
+	Title          string
+	Description    string
+	Body           string
+	TagList        []string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+	Favorited      bool
+	FavoritesCount int
+	Author         Profile
+}
+
+type NewArticle struct {
+	Title       string
+	Description string
+	Body        string
+	TagList     []string
+}
+
+type UpdateArticle struct {
+	Title       string
+	Description string
+	Body        string
+}
+
+type Comment struct {
+	Id        int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Body      string
+	Author    Profile
+}
+
+type NewComment struct {
+	Body string
+}
+
+type GenericErrorModel struct {
+	Errors struct {
+		Body []string
+	}
+}
