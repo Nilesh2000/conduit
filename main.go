0e746f3bcf16b055c7e8e947e230933bc25cd032
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-04-06 15:36:09 +0530

    add validators for register request

--- main.go
+++ main.go
@@ -89,7 +89,11 @@ type UserService interface {
 }
 
 type RegisterRequest struct {
-	User NewUser `json:"user"`
+	User struct {
+		Username string `json:"username" validate:"required"`
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required,min=8"`
+	} `json:"user"`
 }
 
 type UserResponse struct {
