be911ac6f64279849385aedc332988bb7319263a
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-04-07 15:09:48 +0530

    tests for duplicate email and username are now passing

--- internal/repository/postgres/user.go
+++ internal/repository/postgres/user.go
@@ -4,6 +4,8 @@ import (
 	"conduit/internal/repository"
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
 )
 
 type UserRepository struct {
@@ -25,6 +27,16 @@ func (r *UserRepository) Create(username, email, password string) (*repository.U
 	var userID int64
 	err = tx.QueryRow("INSERT INTO users (username, email, password, bio, image, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id", username, email, password, "", "", now, now).Scan(&userID)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code == "23505" {
+				if pqErr.Constraint == "users_username_key" {
+					return nil, repository.ErrDuplicateUsername
+				}
+				if pqErr.Constraint == "users_email_key" {
+					return nil, repository.ErrDuplicateEmail
+				}
+			}
+		}
 		return nil, repository.ErrInternal
 	}
 
