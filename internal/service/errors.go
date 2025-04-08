0b48e7c1d9594ae5413aeca859a51a5f517dd3a5
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-04-09 01:55:30 +0530

    add new error messages for user not found and invalid credentials

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -6,4 +6,7 @@ var (
 	ErrUsernameTaken  = errors.New("username already taken")
 	ErrEmailTaken     = errors.New("email already registered")
 	ErrInternalServer = errors.New("internal server error")
+
+	ErrUserNotFound       = errors.New("user not found")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
