be8f2eeaae751dc96b642cf7a88b468a1c241291
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-04-09 02:13:26 +0530

    add ErrUserNotFound error

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrDuplicateUsername = errors.New("username already exists")
 	ErrDuplicateEmail    = errors.New("email already exists")
 	ErrInternal          = errors.New("internal repository error")
+
+	ErrUserNotFound = errors.New("user not found")
 )
