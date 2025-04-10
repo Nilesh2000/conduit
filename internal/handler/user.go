b6b8fb22104addce75a401ad45aaa03fe6ad0980
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-04-10 09:10:01 +0530

    update comment for GenericErrorModel

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -10,7 +10,7 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
-// GenericErrorModel represents the error response body
+// GenericErrorModel represents the API error response body
 type GenericErrorModel struct {
 	Errors struct {
 		Body []string `json:"body"`
