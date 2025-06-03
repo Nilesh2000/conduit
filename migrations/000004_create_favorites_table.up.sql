7559586e92fe5c858e1f04d8b01d46c73801269a
Nilesh D <42713643+Nilesh2000@users.noreply.github.com>
2025-06-03 12:35:30 +0530

    removing unnecessary trailing comma

--- migrations/000004_create_favorites_table.up.sql
+++ migrations/000004_create_favorites_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE favorites (
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     article_id INTEGER NOT NULL REFERENCES articles(id) ON DELETE CASCADE,
 
-    PRIMARY KEY (user_id, article_id),
+    PRIMARY KEY (user_id, article_id)
 );
